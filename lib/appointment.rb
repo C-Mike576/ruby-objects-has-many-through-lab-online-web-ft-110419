8fe2ffcbf8d1adbdda4b94a3346d23460363b35a
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 19:06:51 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,22 @@
 class Appointment
-  attr_accessor
+  attr_accessor :date
+  attr_reader :patient, :doctor
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def doctor=(doc_obj)
+    @doctor = doc_obj
+  end
+  
+  def patient=(pat_obj)
+    @patient = pat_obj
+  end
+  
+end
