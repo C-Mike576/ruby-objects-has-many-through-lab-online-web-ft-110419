964d4492e7a6cbd69661e77bb5ce59819c125a4d
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 18:57:26 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,18 @@
 class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
   
   
   
