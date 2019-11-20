51b3e197707bb3a90da9260155847bbc9cc0c778
Michael Calkins <m.calkins576@gmail.com>
2019-11-20 19:11:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,6 +16,9 @@ class Patient
     Appointment.all.select { |x| x.patient == self}
   end
   
+  def doctors
+    appointments.collect {|x| x.doctor}
+  
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
